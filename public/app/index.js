382ded897b2b8eb86f87326a515a8eda405aebcf
kimjuneseo <june3929213512@gmail.com>
2022-03-04 19:57:20 +0900

    js 조금

--- public/app/index.js
+++ public/app/index.js
@@ -1,3 +1,12 @@
-const $ = (el) => document.querySelector(el);
+const $ = (el) => document.querySelectorAll(el);
 
-$()
+const keyArr = []; 
+let count = -1;
+const onWindowKeyDown = (e) => {
+    count++;
+    console.log($(".gameBoard .item")[count]);
+    $(".gameBoard .item")[count].innerText = e.key
+    
+};
+
+window.addEventListener("keydown", onWindowKeyDown);
